b7da6e41fb7d268ccea08c04519911042be15e39
林聖泰 <shengtai0201@gmail.com>
2018-01-23 09:14:36 +0800

    ...

---
+++ ShengtaiCore/IAppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shengtai
+{
+    public interface IAppSettings
+    {
+        string DefaultConnection { get; }
+    }
+}
