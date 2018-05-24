48d0dc84f4cb23c56a9de4ad3a244e8499cf668a
林聖泰 <shengtai0201@gmail.com>
2018-05-24 11:11:09 +0800

    rep

---
+++ ShengtaiCore/NpgsqlRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Shengtai.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shengtai
+{
+    public abstract class NpgsqlRepository<TContext, TDefaultConnection> : Repository<NpgsqlConnection, NpgsqlCommand, NpgsqlParameter, NpgsqlDataAdapter, TContext, TDefaultConnection>
+        where TContext : DbContext
+        where TDefaultConnection : IDefaultConnection
+    {
+        protected NpgsqlRepository(IAppSettings<TDefaultConnection> appSettings, TContext dbContext) : base(appSettings, dbContext) { }
+    }
+}
