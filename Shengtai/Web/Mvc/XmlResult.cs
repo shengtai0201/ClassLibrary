2e3e1bcd6d4e21f303461c665b94fdc3e273cd21
林聖泰 <shengtai0201@gmail.com>
2018-09-04 15:43:27 +0800

    xml

---
+++ Shengtai/Web/Mvc/XmlResult.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Shengtai.Web.Mvc
+{
+    public class XmlResult : ActionResult
+    {
+        private readonly object model;
+        public XmlResult(object model)
+        {
+            this.model = model;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (this.model == null)
+                throw new ArgumentNullException();
+
+            context.HttpContext.Response.Clear();
+            context.HttpContext.Response.ContentType = "text/xml";
+
+            var serializer = new System.Xml.Serialization.XmlSerializer(this.model.GetType());
+            serializer.Serialize(context.HttpContext.Response.Output, this.model);
+        }
+    }
+}
