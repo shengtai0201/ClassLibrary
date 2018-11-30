c90212d43cf8d266a2c6827fcd474ad67c245da9
林聖泰 <shengtai0201@gmail.com>
2018-11-30 09:21:52 +0800

    ext

--- ShengtaiCore/DefaultExtensions.cs
+++ ShengtaiCore/DefaultExtensions.cs
@@ -129,7 +129,7 @@ namespace Shengtai
             return result;
         }
 
-        public static void ProcessStart(string fileName, string arguments)
+        public static Process ProcessStart(string fileName, string arguments)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
@@ -163,6 +163,41 @@ namespace Shengtai
             process.WaitForExit();
             process.Close();
             process.Dispose();
+
+            return process;
+        }
+
+        public static async Task<int> ProcessStartAsync(string fileName, string arguments)
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                UseShellExecute = true,
+                CreateNoWindow = false,
+                RedirectStandardOutput = true,
+                Arguments = arguments
+            };
+
+            Process process = Process.Start(startInfo);
+            process.Exited += (sender, args) =>
+            {
+                taskCompletionSource.SetResult(process.ExitCode);
+                process.Dispose();
+            };
+
+            StreamReader reader = process.StandardOutput;
+            string line = null;
+            while (!string.IsNullOrEmpty(line = await reader.ReadLineAsync()))
+                Console.WriteLine(line);
+
+            reader.Close();
+            reader.Dispose();
+
+            process.WaitForExit();
+
+            return await taskCompletionSource.Task;
         }
     }
 }
